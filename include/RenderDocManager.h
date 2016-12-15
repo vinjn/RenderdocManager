18adfdf8ef7425f3d0eb897cef90fe2068a53377
vinjn <vinjn.z@gmail.com>
2016-12-15 13:20:06 +0800

    tab -> spaces

--- include/RenderDocManager.h
+++ include/RenderDocManager.h
@@ -12,7 +12,7 @@ static const char* DEFAULT_RENDERDOC_DLL = "c:/Program Files/RenderDoc/renderdoc
 class RenderDocManager
 {
 public:
-	bool setup(const char* pCapturePath, const char* pRenderDocPath = DEFAULT_RENDERDOC_DLL);
+    bool setup(const char* pCapturePath, const char* pRenderDocPath = DEFAULT_RENDERDOC_DLL);
     ~RenderDocManager();
     void TriggerCapture();
 
@@ -36,27 +36,27 @@ bool RenderDocManager::setup(const char* pCapturePath, const char* pRenderDocPat
     if (RENDERDOC_GetAPI != NULL
         && RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_1_1, (void**)&m_Api) == 1)
     {
-		m_Api->SetLogFilePathTemplate(pCapturePath);
+        m_Api->SetLogFilePathTemplate(pCapturePath);
 
-		RENDERDOC_InputButton captureKey = eRENDERDOC_Key_F12;
-		m_Api->SetCaptureKeys(&captureKey, 1);
-		m_Api->SetFocusToggleKeys(NULL, 0);
+        RENDERDOC_InputButton captureKey = eRENDERDOC_Key_F12;
+        m_Api->SetCaptureKeys(&captureKey, 1);
+        m_Api->SetFocusToggleKeys(NULL, 0);
 
-		m_Api->SetCaptureOptionU32(eRENDERDOC_Option_CaptureCallstacks, 1);
-		m_Api->SetCaptureOptionU32(eRENDERDOC_Option_CaptureAllCmdLists, 1);
-		m_Api->SetCaptureOptionU32(eRENDERDOC_Option_SaveAllInitials, 1);
+        m_Api->SetCaptureOptionU32(eRENDERDOC_Option_CaptureCallstacks, 1);
+        m_Api->SetCaptureOptionU32(eRENDERDOC_Option_CaptureAllCmdLists, 1);
+        m_Api->SetCaptureOptionU32(eRENDERDOC_Option_SaveAllInitials, 1);
 
-		m_Api->MaskOverlayBits(eRENDERDOC_Overlay_None, eRENDERDOC_Overlay_None);
+        m_Api->MaskOverlayBits(eRENDERDOC_Overlay_None, eRENDERDOC_Overlay_None);
 
-		return true;
-	}
-	else
-	{
-		OutputDebugString(L"Render doc API is not compatible !");
-		::FreeLibrary(m_RenderDocDLL);
-		m_RenderDocDLL = NULL;
-		return false;
-	}
+        return true;
+    }
+    else
+    {
+        OutputDebugString(L"Render doc API is not compatible !");
+        ::FreeLibrary(m_RenderDocDLL);
+        m_RenderDocDLL = NULL;
+        return false;
+    }
 }
 
 void RenderDocManager::TriggerCapture()
